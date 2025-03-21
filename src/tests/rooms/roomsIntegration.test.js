771279b9599cb076aaaf9f009611b5e14991886b
yaider <yaiderc19@gmail.com>
2025-03-20 20:23:55 -0500

    feat(rooms): añadiendo tests para encontrar una sola habitacion

--- src/tests/rooms/roomsIntegration.test.js
+++ src/tests/rooms/roomsIntegration.test.js
@@ -1,4 +1,4 @@
-const {KnexRoomRepository, RoomFinder} = require("../../core/rooms");
+const {KnexRoomRepository, RoomFinder, RoomResponse} = require("../../core/rooms");
 const {knexConfig} = require( "../knexfile");
 const RoomsMother = require("./domain/roomsMother");
 const RoomsUpdate = require("../../core/rooms/application/update/RoomUpdate");
@@ -17,6 +17,15 @@ describe('Rooms Integrations testss', () => {
         const createdRoom = await RoomsMother.create(repository)
         const room = (await new RoomFinder(repository).execute(createdRoom.id.value)).toJson().data;
         expect(room.id).toBe(createdRoom.id.value);
+    })
+
+
+    it('Should Find a room by id', async () => {
+        const createdRoom = await RoomsMother.create(repository)
+        const room = await new RoomFinder(repository).execute(createdRoom.id.value);
+        console.log(room)
+        expect(room).not.toBeNull();
+        expect(room).toBeInstanceOf(RoomResponse)
     })
 
     it('Should Update a room', async () => {
